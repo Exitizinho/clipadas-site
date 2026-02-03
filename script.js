70e19dbbf876300822a40af9b7765571be4ae91c
Exitizinho <andre.desporto94@gmail.com>
2026-02-03 23:11:14 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-const track = document.getElementById("carouselTrack");
-const nextBtn = document.getElementById("carouselNext");
-const prevBtn = document.getElementById("carouselPrev");
-
-let index = 0;
-
-const cardWidth = 282; // largura + gap
-const visible = 4;
-
-nextBtn.addEventListener("click", () => {
-  const maxIndex = track.children.length - visible;
-  if (index < maxIndex) {
-    index++;
-    updateCarousel();
-  }
-});
-
-prevBtn.addEventListener("click", () => {
-  if (index > 0) {
-    index--;
-    updateCarousel();
-  }
-});
-
-function updateCarousel() {
-  track.style.transform = `translateX(-${index * cardWidth}px)`;
-}
-
 const track = document.getElementById("videoTrack");
 const slides = document.querySelectorAll(".video-slide");
 const prevBtn = document.querySelector(".video-btn.left");
