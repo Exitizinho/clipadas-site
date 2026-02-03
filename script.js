96aaccb7f8e37a07bb23ad07a0c01b518a74cf60
Exitizinho <andre.desporto94@gmail.com>
2026-02-03 23:05:55 +0000

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,29 @@ prevBtn.addEventListener("click", () => {
 function updateCarousel() {
   track.style.transform = `translateX(-${index * cardWidth}px)`;
 }
+
+const track = document.getElementById("videoTrack");
+const slides = document.querySelectorAll(".video-slide");
+const prevBtn = document.querySelector(".video-btn.left");
+const nextBtn = document.querySelector(".video-btn.right");
+
+let index = 0;
+
+function updateCarousel() {
+  track.style.transform = `translateX(-${index * 100}%)`;
+}
+
+nextBtn.addEventListener("click", () => {
+  if (index < slides.length - 1) {
+    index++;
+    updateCarousel();
+  }
+});
+
+prevBtn.addEventListener("click", () => {
+  if (index > 0) {
+    index--;
+    updateCarousel();
+  }
+});
+
