f9eb288873ae38e08c244b71fbb330fc6b2df78a
Exitizinho <andre.desporto94@gmail.com>
2026-02-07 01:17:15 +0000

    Update gaming.js

--- gaming.js
+++ gaming.js
@@ -1,16 +1,10 @@
 document.querySelectorAll(".gaming-card").forEach(card => {
-  const url = card.dataset.video;
+  const videoUrl = card.dataset.video;
+  if (!videoUrl) return;
 
-  let videoId = null;
+  const videoId = videoUrl.split("v=")[1]?.split("&")[0];
+  if (!videoId) return;
 
-  if (url.includes("watch?v=")) {
-    videoId = url.split("watch?v=")[1].split("&")[0];
-  } else if (url.includes("youtu.be/")) {
-    videoId = url.split("youtu.be/")[1];
-  }
-
-  if (videoId) {
-    const img = card.querySelector("img");
-    img.src = `https://img.youtube.com/vi/${videoId}/hqdefault.jpg`;
-  }
+  const img = card.querySelector("img");
+  img.src = `https://i.ytimg.com/vi/${videoId}/hqdefault.jpg`;
 });
