d277d81182c74553383c4ae2c3618bf3ab4bc5d2
Exitizinho <andre.desporto94@gmail.com>
2026-02-07 01:21:02 +0000

    Update gaming.js

--- gaming.js
+++ gaming.js
@@ -1,10 +1,23 @@
-document.querySelectorAll(".gaming-card").forEach(card => {
-  const videoUrl = card.dataset.video;
-  if (!videoUrl) return;
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll("[data-video]").forEach(card => {
+    const url = card.dataset.video;
 
-  const videoId = videoUrl.split("v=")[1]?.split("&")[0];
-  if (!videoId) return;
+    let videoId = null;
 
-  const img = card.querySelector("img");
-  img.src = `https://i.ytimg.com/vi/${videoId}/hqdefault.jpg`;
+    // youtu.be/ID
+    if (url.includes("youtu.be/")) {
+      videoId = url.split("youtu.be/")[1].split("?")[0];
+    }
+
+    // youtube.com/watch?v=ID
+    if (url.includes("watch?v=")) {
+      videoId = url.split("watch?v=")[1].split("&")[0];
+    }
+
+    if (!videoId) return;
+
+    const img = card.querySelector("img");
+    img.src = `https://i.ytimg.com/vi/${videoId}/hqdefault.jpg`;
+    img.loading = "lazy";
+  });
 });
