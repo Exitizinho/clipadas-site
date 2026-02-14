2e98f1d77906f11873f11ae03f67e3844c7acbb7
Exitizinho <andre.desporto94@gmail.com>
2026-02-14 16:04:59 +0000

    Update gaming.js

--- gaming.js
+++ gaming.js
@@ -4,31 +4,26 @@ document.addEventListener("DOMContentLoaded", () => {
      THUMBNAILS YOUTUBE
   ============================ */
   document.querySelectorAll("[data-id]").forEach(card => {
-    const url = card.dataset.id;
-    let videoId = null;
 
-    if (url.includes("youtu.be/")) {
-      videoId = url.split("youtu.be/")[1].split("?")[0];
-    }
-    if (url.includes("watch?v=")) {
-      videoId = url.split("watch?v=")[1].split("&")[0];
-    }
-    if (!videoId) return;
+  const videoId = card.dataset.id;
+  if (!videoId) return;
 
-    const img = card.querySelector("img");
-    if (!img) return;
+  const img = card.querySelector("img");
+  if (!img) return;
 
-    const maxRes = `https://i.ytimg.com/vi/${videoId}/maxresdefault.jpg`;
-    const hqRes  = `https://i.ytimg.com/vi/${videoId}/hqdefault.jpg`;
+  const maxRes = `https://i.ytimg.com/vi/${videoId}/maxresdefault.jpg`;
+  const hqRes  = `https://i.ytimg.com/vi/${videoId}/hqdefault.jpg`;
 
-    const testImg = new Image();
-    testImg.src = maxRes;
-    testImg.onload = () => img.src = maxRes;
-    testImg.onerror = () => img.src = hqRes;
+  const testImg = new Image();
+  testImg.src = maxRes;
 
-    img.loading = "lazy";
-    img.decoding = "async";
-  });
+  testImg.onload = () => img.src = maxRes;
+  testImg.onerror = () => img.src = hqRes;
+
+  img.loading = "lazy";
+  img.decoding = "async";
+});
+
 
   /* ===========================
      FUNÇÃO NORMALIZAR TEXTO
