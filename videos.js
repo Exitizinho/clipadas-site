dbeb3557cd20be16382b03c37cf47f57c741d6e2
Exitizinho <andre.desporto94@gmail.com>
2026-02-15 21:47:06 +0000

    Update videos.js

--- videos.js
+++ videos.js
@@ -33,3 +33,66 @@ function loadVideos(pageName, containerId) {
     });
 }
 
+function loadHomeVideos() {
+
+  fetch("videos.json")
+    .then(res => res.json())
+    .then(videos => {
+
+      // HERO
+      const heroTrack = document.getElementById("videoTrack");
+
+      const featured = videos.filter(v => v.featured);
+
+      heroTrack.innerHTML = featured.map(video => `
+        <div class="video-slide" data-id="${video.id}">
+          <img class="hero-thumb">
+          <div class="hero-overlay">
+            <div class="hero-play hero-play-center">▶</div>
+            <div class="hero-info">
+              <h2>${video.title}</h2>
+              <span>${video.channel}</span>
+            </div>
+          </div>
+        </div>
+      `).join("");
+
+      document.querySelectorAll(".video-slide").forEach(slide => {
+
+        const id = slide.dataset.id;
+        const img = slide.querySelector("img");
+
+        img.src = `https://img.youtube.com/vi/${id}/maxresdefault.jpg`;
+
+        img.onerror = () => {
+          img.src = `https://img.youtube.com/vi/${id}/hqdefault.jpg`;
+        };
+
+      });
+
+
+      // ÚLTIMOS VIDEOS
+      const latest = document.getElementById("latestVideos");
+
+      const latestVideos = videos
+        .sort((a,b) => new Date(b.date) - new Date(a.date))
+        .slice(0, 6);
+
+      latest.innerHTML = latestVideos.map(video => `
+        <div class="video-card" data-video="${video.id}">
+          <div class="thumb">
+            <img src="https://img.youtube.com/vi/${video.id}/hqdefault.jpg">
+            <span class="play">▶</span>
+          </div>
+          <div class="info">
+            <h3>${video.title}</h3>
+            <span>${video.channel}</span>
+          </div>
+        </div>
+      `).join("");
+
+    });
+
+}
+
+
