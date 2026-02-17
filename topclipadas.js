0bcef206596a58536ad9b58a561f0af4948d8ec9
Exitizinho <andre.desporto94@gmail.com>
2026-02-17 21:49:40 +0000

    Add files via upload

--- topclipadas.js
+++ topclipadas.js
@@ -34,14 +34,14 @@ function loadTopClipadas(subcategory) {
         </iframe>
       `
       : `
-        <img src="https://i.ytimg.com/vi/${video.id}/oardefault.jpg">
+        <img src="https://i.ytimg.com/vi/${video.id}/maxresdefault.jpg">
         <span class="play">▶</span>
       `;
 
  return `
   <div class="video-card ${video.platform === 'youtube' ? 'short-card' : 'twitch-card'}"
-    data-id="${video.id}"
-    data-platform="${video.platform}">
+  data-id="${video.id}"
+  data-platform="${video.platform}">
    <div class="thumb ${video.subcategory === 'shorts' ? 'vertical' : ''}">
 ${media}
 </div>
