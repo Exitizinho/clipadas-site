a2944d9e65461f476c788c33d00a1e3d61c5c95c
Exitizinho <andre.desporto94@gmail.com>
2026-02-17 04:16:07 +0000

    Update videos.js

--- videos.js
+++ videos.js
@@ -33,41 +33,6 @@ function loadVideos(pageName, containerId) {
     });
 }
 
-function loadVideos(pageName, containerId) {
-
-  fetch("videos.json")
-    .then(res => res.json())
-    .then(videos => {
-
-      const filtered = videos.filter(video =>
-        video.page === pageName
-      );
-
-      const container = document.getElementById(containerId);
-
-      if (filtered.length === 0) {
-        container.innerHTML = "<p>Sem vídeos ainda 😅</p>";
-        return;
-      }
-
-      filtered.forEach(video => {
-        container.innerHTML += `
-          <a href="#" class="video-card" data-id="${video.id}">
-            <div class="thumb">
-              <img src="https://img.youtube.com/vi/${video.id}/hqdefault.jpg">
-              <span class="play">▶</span>
-            </div>
-            <div class="info">
-              <h3>${video.title}</h3>
-              <span>${video.channel}</span>
-            </div>
-          </a>
-        `;
-      });
-
-    });
-}
-
 function loadHome() {
 
   fetch("videos.json")
