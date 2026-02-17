7e859ae9ae5223c34c4527efdb08eba96f51b8fc
Exitizinho <andre.desporto94@gmail.com>
2026-02-17 19:20:47 +0000

    Update topclipadas.js

--- topclipadas.js
+++ topclipadas.js
@@ -39,9 +39,9 @@ function loadTopClipadas(subcategory) {
       `;
 
  return `
-  <div class="video-card"
-    data-id="${video.id}"
-    data-platform="${video.platform}">
+  <div class="video-card ${video.platform === 'youtube' ? 'short-card' : 'twitch-card'}"
+  data-id="${video.id}"
+  data-platform="${video.platform}">
    <div class="thumb ${video.subcategory === 'shorts' ? 'vertical' : ''}">
 ${media}
 </div>
